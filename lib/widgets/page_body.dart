6058273656abde82f26b16afd64dc44c3030cc8d
henryadebayo <henryadebayo250@gmail.com>
2022-02-04 00:01:23 +0100

    receiving sport data

---
+++ lib/widgets/page_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:sportapp_test/services/data_Source.dart';
+
+
+
+Widget? pageBody(List<SoccerApi> allMatches){
+ return Column(children: [
+
+],);
+
+}
